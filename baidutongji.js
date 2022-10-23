5fbb884614daff65a37820c620bd8c411e0667dc
xyz8848 <xyz8848git@outlook.com>
2022-10-23 11:36:35 +0800

    baidutongji

---
+++ baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?de7dc81046a3b33a05f45db9d51296f8";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
